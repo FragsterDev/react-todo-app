60d6510bf661ab36cdf2d55ac81a7f74beab1f15
Pratik Sharma <pratiksharma@Pratiks-Mac-mini.local>
2025-01-31 19:02:19 +0530

    latest

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,60 @@
-import TodoInput from "./components/TodoInput"
-import TodoList from "./components/TodoList"
-import { useState } from "react"
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import { useEffect, useState } from "react";
 
 function App() {
-  
-  const [todos, setTodo] = useState(
-    [
-      'Go to gym',
-      'Have breakfast',
-    ]
-  )
+  const [todos, setTodos] = useState([
+    "Go to gym",
+    "Have breakfast",
+  ]);
 
-  const [todoValue, setTodoValue] = useState('')
+  const [todoValue, setTodoValue] = useState("");
 
-  function handleAddTodo(newTodo){
-    const newTodoList = [...todos, newTodo]
-    persistData(newTodoList)
-    setTodo(newTodoList)
+  function handleAddTodo(newTodo) {
+    const newTodoList = [...todos, newTodo];
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
-  function handleDeleteTodo(index){
-    const newTodoList = todos.filter((todo, todoIndex) => {
-      return todoIndex !== index
-    })
-    setTodo(newTodoList);
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((_, todoIndex) => todoIndex !== index);
+    persistData(newTodoList);
+    setTodos(newTodoList);
   }
 
-  function handleEditTodo(index){
-    const valueToBeEdited = todos[index]
-    setTodoValue(valueToBeEdited)
-    handleDeleteTodo(index)
-
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index];
+    setTodoValue(valueToBeEdited);
+    handleDeleteTodo(index);
   }
+
+  useEffect(() => {
+    let localTodos = localStorage.getItem("todos");
+    if (!localTodos) {
+      return;
+    }
+    localTodos = JSON.parse(localTodos).todos;
+    setTodos(localTodos);
+  }, []);
 
   function persistData(newList) {
-    localStorage.setItem('todos', JSON.stringify({todos: newList}))
+    localStorage.setItem("todos", JSON.stringify({ todos: newList }));
   }
 
   return (
     <main>
-      <TodoInput handleAddTodo={handleAddTodo} setTodoValue={setTodoValue} todoValue = {todoValue}/>
-      <TodoList todos={todos} handleDeleteTodo={handleDeleteTodo} handleEditTodo={handleEditTodo}/>
+      <TodoInput
+        handleAddTodo={handleAddTodo}
+        setTodoValue={setTodoValue}
+        todoValue={todoValue}
+      />
+      <TodoList
+        todos={todos}
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+      />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
